5d87361693e0332a88145ee5646904819ae291f7
dom <githubaccount56556@proton.me>
2026-04-30 16:57:23 +0800

    do not exit onDestroy
    Signed-off-by: dom <githubaccount56556@proton.me>

--- android/app/src/main/kotlin/com/example/piliplus/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/piliplus/MainActivity.kt
@@ -251,8 +251,6 @@ class MainActivity : AudioServiceActivity() {
     override fun onDestroy() {
         stopService(Intent(this, com.ryanheise.audioservice.AudioService::class.java))
         super.onDestroy()
-        android.os.Process.killProcess(android.os.Process.myPid())
-        exitProcess(0)
     }
 
     override fun onUserLeaveHint() {
