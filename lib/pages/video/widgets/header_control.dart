6a7ce57708c3cc240d8fe0cae3800f708188986d
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2026-08-22 10:36:14 +0000

    fix: copy videoParams (#2633)

--- lib/pages/video/widgets/header_control.dart
+++ lib/pages/video/widgets/header_control.dart
@@ -845,7 +845,7 @@ class HeaderControlState extends State<HeaderControl>
                       title: const Text("VideoTrack"),
                       subtitle: Text(state.track.video.toString()),
                       onTap: () =>
-                          Utils.copyText('VideoTrack\n${state.track.audio}'),
+                          Utils.copyText('VideoTrack\n${state.track.video}'),
                     ),
                     ListTile(
                       dense: true,
@@ -856,7 +856,7 @@ class HeaderControlState extends State<HeaderControl>
                     ListTile(
                       dense: true,
                       title: const Text("Volume"),
-                      subtitle: Text(volume.toString()),
+                      subtitle: Text(volume),
                       onTap: () => Utils.copyText('Volume\n$volume'),
                     ),
                     ListTile(
