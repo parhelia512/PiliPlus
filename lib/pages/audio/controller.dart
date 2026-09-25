df86852f3ad7c200fd5f2dccca917dd213c9827f
dom <githubaccount56556@proton.me>
2026-09-25 10:49:48 +0800

    opt update PlaybackState
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/pages/audio/controller.dart
+++ lib/pages/audio/controller.dart
@@ -828,7 +828,8 @@ class AudioController extends GetxController
       ?..onPlay = null
       ..onPause = null
       ..onSeek = null
-      ..onVideoDetailDispose(hashCode.toString());
+      ..onVideoDetailDispose(hashCode.toString())
+      ..clearIfNeeded();
     _subscriptions?.forEach((e) => e.cancel());
     _subscriptions?.clear();
     _subscriptions = null;
